0fe53fc51ad9c00955746ab04c007810bd1e076d
Mariel Miscavage <memiscavage@gmail.com>
2024-06-08 14:46:58 -0500

    moved mongo uri to the .env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
