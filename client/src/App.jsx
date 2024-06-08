924ea01a0e4ed3a9938b1a9819bf1f36184a9278
Mariel Miscavage <memiscavage@gmail.com>
2024-06-08 14:49:22 -0500

    created apollo provider and made httplink and apollolink to request apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,36 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache, concat } from '@apollo/client';
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => {
+    const token = localStorage.getItem('authjwt')
+    return {
+      headers: {
+        ...headers,
+        authorization: token?`Bearer ${token}`: null
+      }
+    }
+  })
+  return forward(operation)
+})
+
+const httpLink = new HttpLink({
+  uri: '/graphql'
+})
+
+const client = new ApolloClient({
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
